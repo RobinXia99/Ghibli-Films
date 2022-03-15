e0435116c58996a3a8aa59c95203829b848e3526
RobinXia99 <>
2022-03-16 00:22:04 +0100

    Display film info

--- films.js
+++ films.js
@@ -10,6 +10,8 @@ const filmCatalog = document.getElementById('scrollbar');
 
 const background = document.getElementById("backgroundCover");
 
+const filmInfo = document.getElementById("filmInfo_wrapper");
+
 for(let navItem of navItems) {
     if(activePage.includes(navItem.innerText.toLowerCase())) {
         navItem.classList.add("active");
@@ -35,8 +37,10 @@ let films = {
 
         background.src = `images/${film.id}.png`;
 
-        
-        
+        let index = (this.list).findIndex(function(item, index) {
+            return item.id === film.id;
+        })
+        setInfo(this.list[index]);
        
     },
 };
@@ -44,7 +48,6 @@ let films = {
 window.addEventListener('load', async e => {
 
     films.list = await fetchFilms(filmsUrl);
-
 
     for(let film of films.list) {
 
@@ -79,9 +82,107 @@ async function fetchFilms(url) {
 }
 
 
+function setInfo(film) {
 
+    filmInfo.innerHTML = "";
 
+    let infoTypes = ['Director', 'Producer', 'Release Date', 'Running Time', 'Rating'];
 
+    let filmTitle = document.createElement('header');
+
+    let filmCover = document.createElement('img');
+
+    let filmTable = document.createElement('table');
+
+    let tableBody = document.createElement('tbody');
+
+    let synopsis = document.createElement('p');
+
+    filmTitle.classList.add('filmTitle');
+    filmTitle.innerText = `${film.title} - ${film.original_title}`
+
+    filmCover.classList.add('filmCover');
+    filmCover.src = film.image;
+
+    for(let type of infoTypes) {
+        let value;
+        switch (type) {
+            case 'Director':
+                value = film.director;
+                break;
+            case 'Producer':
+                value = film.producer;
+                break;
+            case 'Release Date':
+                value = film.release_date;
+                break;
+            case 'Running Time':
+                value = `${film.running_time}min`;
+                break;
+            case 'Rating':
+                value = `${film.rt_score}/100`;
+                break;
+        }
+
+        let tableRow = document.createElement('tr');
+        let tableDataType = document.createElement('td');
+        let tableData = document.createElement('td');
+
+        tableDataType.innerText = type;
+        tableData.innerText = value;
+
+        tableRow.appendChild(tableDataType);
+        tableRow.appendChild(tableData);
+        
+        tableBody.appendChild(tableRow);
+    }
+
+    filmTable.classList.add('film_infoTable');
+    filmTable.appendChild(tableBody);
+
+    synopsis.classList.add('film_synopsis');
+    synopsis.innerText = film.description;
+
+    filmInfo.append(filmTitle, filmCover, filmTable, synopsis);
+
+    
+}
+
+` <header id="filmTitle">Ponyo - 崖の上のポニョ</header>
+<img id="filmCover" src="ponyocover.png" alt="">
+<table class="film_infoTable">
+    <tbody>
+        <tr>
+            <td>Director</td>
+            <td id="directorsName">Name</td>
+        </tr>
+        <tr>
+            <td>Producer</td>
+            <td id="producersName">Name</td>
+        </tr>
+        <tr>
+            <td>Release Date</td>
+            <td id="releaseDate">1999</td>
+        </tr>
+        <tr>
+            <td>Running Time</td>
+            <td id="runTime">120min</td>
+        </tr>
+        <tr>
+            <td>Rating</td>
+            <td id="rating">10/10</td>
+        </tr>
+    </tbody>
+</table>
+<p id="filmSynopsis">
+    The son of a sailor, 5-year old Sosuke lives a quiet life on an oceanside cliff with his mother Lisa. 
+    One fateful day, he finds a beautiful goldfish trapped in a bottle on the beach and upon rescuing her, 
+    names her Ponyo. But she is no ordinary goldfish. The daughter of a masterful wizard and a sea goddess, 
+    Ponyo uses her father's magic to transform herself into a young girl and quickly falls in love with Sosuke, 
+    but the use of such powerful sorcery causes a dangerous imbalance in the world. 
+    As the moon steadily draws nearer to the earth and Ponyo's father sends the ocean's mighty waves to find his daughter, 
+    the two children embark on an adventure of a lifetime to save the world and fulfill Ponyo's dreams of becoming human.
+</p>`
 
 
 
